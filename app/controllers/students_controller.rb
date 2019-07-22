59a03aabcb8b748ee88631cecc59478acc9a765a
Savannah C <sgcorrell@gmail.com>
2019-07-22 19:38:38 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,9 +16,9 @@ class StudentsController < ApplicationController
     end
     
     def activate
-    @student = Student.find(params[:id])
-    @student.active = !@student.active
-    @student.save
-    redirect_to student_path(@student)
+      @student = Student.find(params[:id])
+      @student.active = !@student.active
+      @student.save
+      redirect_to student_path(@student)
   end
 end
